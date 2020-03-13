5a11bc24a8a2c9c7da912eb0617893f0128831ba
Victor <vop@list.ru>
2020-03-13 14:08:45 +0300

    dz3.1

--- Lesson3.py
+++ Lesson3.py
@@ -7,10 +7,11 @@ list_txt = list(origin)
 print(len(list_txt),type(list_txt))
 
 # Собираем знаки пунктуации в кучу
-marks = ['"','.','!','?','-',':',';',',','(',')']
+marks = ['«','»','.','!','?','—',':',';',',','(',')']
 
-# Перебираем файл для анализа на знаки и удаляем их
-for i in range(len(list_txt)):
-    print(list_txt[i])
-        if list_txt[i] in marks:
-        list.txt=list_txt.remove(i)
+# Перебираем текст для анализа на знаки и создаем новый текст
+new_list = []
+for e in list_txt:
+    if e not in marks:
+        new_list.append(e)
+print(''.join(new_list))
