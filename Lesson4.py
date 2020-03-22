df0eab913be5cdd875a27eda4a693f45e09c296e
VKeiby <vop@list.ru>
2020-03-22 17:30:15 +0300

    4.5

--- Lesson4.py
+++ Lesson4.py
@@ -9,7 +9,7 @@ def f(names,intX):
         integer_list.append(names[rnd])
     return integer_list
 
-def topWord(integer_list):
+def countNsort(integer_list):
     DictOut = {}
     for key in integer_list:
         if not key in DictOut:
@@ -17,33 +17,23 @@ def topWord(integer_list):
         else:
             DictOut[key] += 1
    # -------------Freq Words
-    SortDict = list(DictOut.items())
-    SortDict.sort(key=lambda i: i[1], reverse=True)
-    topWords = SortDict[0]
-    print('Самое популярное слово = ', topWords)
-    return topWords
+    Dict = list(DictOut.items())
+    Dict.sort(key=lambda i: i[1], reverse=True)
+    return Dict
 
-def lessLetter(integet_list):
+def topWord(integer_list):
+    print('Самое популярное слово = ', sortedDict[0])
+def lessLetter(sortedDict):
+    letter_tmp=str(sortedDict[-1])
+    print('Самая редкая буква имени = ', letter_tmp[2])
 
-    return
-
-
-
-
-
-
-dbase = ['Yana','Marina','Vasiliy','Alex','Egor','Yaroslav','Dariya','Nina','Katerine','Fodor','Anton','Victoria',
+dbase = ['Yana','Marina','Vasiliy','Alex','Egor','Yaroslav','Dariya','Nina','Katerine','Fedor','Anton','Victoria',
          'Vera','Boris','Yasha','Grisha','Tolik','Timofey','Sandra','Nastya','Kseniya','Kristina','Polina','Tanya']
 
+outList=(f(dbase,105))
+sortedDict=countNsort(outList)
+topWord(sortedDict)
+lessLetter(sortedDict)
 
-print(f(dbase,5))
-
-topWord(f(dbase,100))
-
-print()
-
-# -------------..2..-----------
-
-# -------------..3..-----------
 
 # ------------..PRO..----------
