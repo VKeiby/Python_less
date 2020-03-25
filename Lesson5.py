8a8eeb49a54d12fc45b3dd91a580619b3f9b68a3
Victor <vop@list.ru>
2020-03-25 13:33:50 +0300

    time

---
+++ Lesson5.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+# ________PROC________
+
+NUM=int(input('Input number 1-1000, for stop inter 0: '))
+while NUM != 0:
+    print('Number entered= ',NUM)
+    # SimpleNum()
+    # DivisionNum()
+    # MaxDivNum()
+    # CanonicNum()
+    NUM = int(input('One more? For stop inter 0: '))
+print('TNX,Have a nice day!')
+
