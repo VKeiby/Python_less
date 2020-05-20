f0816a8471964700a12f80a3845f5d1aa84db538
Victor <vop@list.ru>
2020-05-20 16:15:20 +0300

    time

--- NeuralColab/Panda3.py
+++ NeuralColab/Panda3.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 
 data = pd.read_csv('train_test.csv', index_col = 0)
 data_backup = data.copy()
@@ -14,12 +14,31 @@ for el in lCol:
   if name > 200:
     data1 = data1.drop(columns=[el])
 
-# print(data.describe())
-print(data1.head())
-
 data1.dropna(axis = 0, how ='any', inplace=True)
-print(data1.info())
-print(data1.isna().sum().sum())
+
+# Задача 5
+# Данные, с которыми мы работаем в этом домашнем задании, содержат информацию о проданных домах.
+# В табличке из прошлой задачи найдите все дома,
+# проданные в 2007 году, у которых цена продажи выше 300000 и есть бассейн.
+# Для подвыборки с указанными условиями сначала создайте маску.
+#
+# цена продажи SalePrice
+# год продажи YrSold
+# наличие бассейна можно определить по столбцу PoolArea
+
+#задаем маску
+priceMask = data1['SalePrice'] > 300000
+YrMask = data1['YrSold'] == 2007
+PoolMask = data1['PoolArea'] == True
+
+print(data1.loc[1170:1200, ['SalePrice','YrSold','PoolArea']])
+
+# print(priceMask.head(20))
+# print(YrMask.head(20))
+# print(PoolMask.head(20))
+
+
+
 
 # fig, ax = plt.subplots(figsize=(20,12))
 # sns_heatmap = sns.heatmap(data1.isnull(), yticklabels=False, cbar=False, cmap='viridis')
