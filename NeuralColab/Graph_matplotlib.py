6c5a1df4fe18c1f3af906c2f985202097e4e0c0a
VKeiby <vop@list.ru>
2020-06-03 00:52:31 +0300

    seaborn

--- NeuralColab/Graph_matplotlib.py
+++ NeuralColab/Graph_matplotlib.py
@@ -14,10 +14,23 @@ db1_backup = db1.copy()
 db1.index = pd.to_datetime(db1.date)
 db1.drop('date', axis=1, inplace=True)
 print(db1.head())
-
+# y = db1.Occupancy
+# print(y)
 # Постройте график всех парных взаимосвязей с разметкой цвета в соответствии с метками классов
 # (столбец Occupancy)
 #
 # P.S. используйте функцию sns.pairplot c параметром hue
 
-sns.set_style('darkgrid')
+# sns.set_style('darkgrid')
+# sns.lmplot(data=db1, x=db1.columns[0], y=db1.columns[1], hue='Occupancy')    # db1.columns[0] - это значения столбика Temperature
+#                                                             # db1.columns[1] - это значения столбика Humidity
+# plt.show()
+
+# **********************************
+
+plt.figure(figsize=(12,6))
+
+# график boxplot показывает средний, нижний и верхний квартили,
+# минимальное и максимальное значение выборки и выбросы
+sns.boxplot(data=db1, palette=sns.color_palette('Greens'), orient='h')
+plt.show()
