9b0793e2905c2dc50d7414d5ea9eb8f84b3b8c61
Victor <vop@list.ru>
2021-09-06 10:40:19 +0300

    parser

---
+++ OOP/Example/class_person.py
@@ -0,0 +1,9 @@
+class Person:
+
+    def __init__(self, name = 'Guest'):
+        self.name = name
+
+    def info(self):
+        print(f'Hello, my name is {self.name}')
+
+
---
+++ OOP/Example/lesson37.py
@@ -0,0 +1,10 @@
+from class_person import Person
+
+person1 = Person()
+person1.info()
+
+person2 = Person('John')
+person2.info()
+
+person3 = Person('katy')
+person3.info()
