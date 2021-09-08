bda0859d94c90cff117141ca1e3767d20fddfe2a
Victor <vop@list.ru>
2021-09-08 13:37:56 +0300

    rbcnews 0.3

--- MyProg/rbcnews.py
+++ MyProg/rbcnews.py
@@ -6,21 +6,23 @@ html = req.read()
 
 soup = BeautifulSoup(html, 'html.parser')
 news = soup.find_all('span', class_='main__feed__title')
+newsPlus = soup.find_all('div', class_='news-feed__wrapper')
 
 results = []
-# print(news)
+# print(newsPlus)
 
-for item in news:
-    print(item.text)
-    # title = item.find('span', class_='data-vr-headline').get_text(strip=True)
+for item in newsPlus:
+    # print(item.text)
+    title = item.find('span', class_='news-feed__item__title').get_text(strip=True)
     # desc = item.find('span', class_='name-dop').get_text(strip=True)
     # href = item.a.get('href')
     results.append({
-        'title': item
+        'title': item.text
         # 'desc': desc,
         # 'href': href,
     })
-# print(results)
+for sTr in results:
+    print(sTr)
 # f = open('news.txt', 'w', encoding='utf-8')
 # i = 1
 # for item in results:
