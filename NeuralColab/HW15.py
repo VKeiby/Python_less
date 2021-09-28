cc4ea2ead0598eb15a711732ce281ae8e6773b67
Victor <vop@list.ru>
2021-09-28 11:04:21 +0300

    hw15.accur

--- NeuralColab/HW15.py
+++ NeuralColab/HW15.py
@@ -16,6 +16,22 @@ import matplotlib.pyplot as plt #Отрисовка изображений
 from PIL import Image #Отрисовка изображений
 #Отрисовывать изображения в ноутбуке, а не в консоль или файл
 # %matplotlib inline
+
+# Функция пересоздаёт пустую сеть
+def createModel():
+
+  # Создаем сеть
+  model = Sequential()
+
+  # Добавляем слои
+  model = Sequential()
+  model.add(Dense(800, input_dim=784, activation='relu'))
+  model.add(Dense(10, activation='softmax'))
+  model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+
+
+  return model # Возвращаем созданную сеть
+
 (x_train_org, y_train_org), (x_test_org, y_test_org) = mnist.load_data() #Загрузка данных Mnist
 #Меняем формат входных картинок с 28х28 на 784х1
 x_train = x_train_org.reshape(60000, 784)
@@ -48,3 +64,40 @@ for el in expl:
 
     df = pd.DataFrame(data, columns = ['model', 'size_data','val_accuracy'])
 print(df)
+
+# Вычисляем результаты сети на тестовом наборе
+scores = model.evaluate(x_test, y_test, verbose=1)
+
+# scores состоит из двух знанчений
+# scores[0] - loss сети на тестовой выборке
+# scores[1] - процент правильно распознанных примеров на тестовой выборке
+print(scores)
+print("Доля верных ответов на тестовых данных, в процентах: ", round(scores[1] * 100, 4), "%", sep="")
+
+# Создаём пустую сеть
+model = createModel()
+
+# Обучаем сеть
+history01 = model.fit(x_train,
+                    y_train,
+                    batch_size=8,
+                    epochs=4,
+                    validation_split=0.2,
+                    verbose=1)
+
+# Выводим график точности на обучающей выборке
+# label - имя графика в легенде
+plt.plot(history01.history['accuracy'],
+         label='Доля верных ответов на обучающем наборе')
+
+# Выводим график точности на проверочной выборке
+plt.plot(history01.history['val_accuracy'],
+         label='Доля верных ответов на проверочном наборе')
+
+# Выводим подписи осей
+plt.xlabel('Эпоха обучения')
+plt.ylabel('Доля верных ответов')
+
+# Выводим легенду
+plt.legend()
+plt.show()
