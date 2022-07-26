ca9f8444d4633266cd83ac4e5ea9c4259e5cf813
ProINIT KB <victor@proinit.md>
2022-07-26 18:06:29 +0300

    ruckus26jul 18:00

--- MyProg/ParseTable.py
+++ MyProg/ParseTable.py
@@ -3,9 +3,7 @@ import numpy as np
 from pyparsing import *
 
 # Pyparsing Learn https://habr.com/ru/post/239081/
-
 # b = 'Interface GigabitEthernet5/1/15(): Ethernet speed <1Gbps'
-#
 # interface = Word(alphas) # Only int name
 # portNum = Word(nums + '/')
 # GREEDYDATA = 'Ethernet speed <1Gbps'
@@ -13,23 +11,20 @@ from pyparsing import *
 # res = parse_module.parseString(b)
 # res[1] = 'ethernet'
 # print(' '.join(res))
-
 # -------           END                  ---------
 
 data = pd.read_csv('zbx_problems_export.csv',dtype=object) # load data
-print(data.head(1)) # control view
+# print(data.head(1)) # control view
 LinkTable = data[['Host','Problem']]
 
 # There is can be subprogram...
 interface = Word(alphas) # Only int name
 portNum = Word(nums + '/')
-GREEDYDATA = 'Ethernet speed <1Gbps'
-parse_module = 'Interface' + interface + portNum + Suppress('():') + Suppress(GREEDYDATA)
+GREEDYDATA = restOfLine()
+parse_module = 'Interface' + interface + portNum + Suppress(GREEDYDATA)
 for i in range (15):
-    res = parse_module.parseString(LinkTable.iloc[i,1])
+    res = parse_module.parseString(LinkTable['Problem'].iloc[i])
     res[1] = 'ethernet'
     res = ' '.join(res)
-    LinkTable.loc[i,1] = res
-print (sum)
-print(LinkTable.shape)
-print(LinkTable.head(15))
+    LinkTable.iloc[i,1] = res
+print(LinkTable.head())
