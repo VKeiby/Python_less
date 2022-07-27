80f47064a88d4f28d7b4fa2e2adbb47714822316
ProINIT KB <victor@proinit.md>
2022-07-27 17:44:02 +0300

    ruckus27jul 17:45 https://pandas.pydata.org/docs/user_guide/10min.html

--- MyProg/ParseTable.py
+++ MyProg/ParseTable.py
@@ -1,6 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+
+import numpy
+import paramiko
+import os
+import cmd
+import datetime
 import pandas as pd
 import numpy as np
-from pyparsing import *
+from pyparsing import Word, alphas, restOfLine, Suppress, nums
 
 # Pyparsing Learn https://habr.com/ru/post/239081/
 # b = 'Interface GigabitEthernet5/1/15(): Ethernet speed <1Gbps'
@@ -11,10 +21,12 @@ from pyparsing import *
 # res = parse_module.parseString(b)
 # res[1] = 'ethernet'
 # print(' '.join(res))
-# -------           END                  ---------
+# -------                 END                  ---------
 
-data = pd.read_csv('zbx_problems_export.csv',dtype=object) # load data
-# print(data.head(1)) # control view
+data = pd.read_csv('zbx_problems_export.csv', dtype=object) # load data
+# print(data) # control view
+# print (data.columns)
+
 LinkTable = data[['Host','Problem']]
 
 # There is can be subprogram...
@@ -23,10 +35,62 @@ portNum = Word(nums + '/')
 GREEDYDATA = restOfLine()
 parse_module = 'Interface' + interface + portNum + Suppress(GREEDYDATA)
 for i in range (15):
+    # https://pandas.pydata.org/docs/user_guide/10min.html    #How to change & read cell in dataframe
     res = parse_module.parseString(LinkTable['Problem'].iloc[i])
     res[1] = 'ethernet'
     res = ' '.join(res)
     LinkTable.iat[i,1] = res
-print(LinkTable.head())
-print(data.head())
-# https://pandas.pydata.org/docs/user_guide/10min.html
+
+print(LinkTable)
+
+
+# ---------------------------------------------------------------------
+###set date and time
+now = datetime.datetime.now()
+
+###authentication
+USER = 'bsquared'
+PASSWORD = 'bsquared2019!@#'
+secret = 'bsquared2019!@#'
+
+###start FOR ...in
+# f = open('LinkTable.csv')
+# for ip in f.readlines():
+#     ip = ip.strip()
+#     ###prefix files for backup  - filename_prefix = '/var/netbackup/' + ip
+#     filename_prefix = 'C:/Repo/Python_less/MyProg/' + ip # TMP for debug
+#
+#     ###session start
+#     client = paramiko.SSHClient()
+#     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#     client.connect(ip, username=USER, password=PASSWORD)
+#
+#     ###ssh shell
+#     chan = client.invoke_shell()
+#     time.sleep(2)
+#     ###enter enable secret
+#     chan.send('ena\n')
+#     chan.send(USER + '\n')
+#     chan.send(secret + '\n')
+#     time.sleep(1)
+#     ###terminal lenght for no paging
+#     chan.send('skip\n')
+#     time.sleep(1)
+#     ###show config and write output
+#     chan.send('sh run\n')
+#     time.sleep(10)
+#     output = chan.recv(999999)
+#     ###show output config and write file with prefix, date and time
+#     print(output)
+#     # filename = "%s_%.2i-%.2i-%i_%.2i-%.2i-%.2i" % (filename_prefix,now.day,now.month,now.year,now.hour,now.minute,now.second)
+#     filename = "%s_%.2i%.2i%i_%.2i%.2i%.2i" % (
+#     filename_prefix, now.year, now.month, now.day, now.hour, now.minute, now.second)
+#     ff = open(filename, 'a')
+#     # ff.write(output)
+#     ff.write(output.decode("utf-8"))
+#     ff.close()
+#     ###close ssh session
+#     client.close()
+#
+#     print(ip)
+#     f.close()
